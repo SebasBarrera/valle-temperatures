a554e4b8f7eeb9a0a305dda3989b95bb6a340f6b
Sebastián Barrera <juan.barrera4@correo.icesi.edu.co>
2021-02-21 12:04:00 -0500

    Template and dataset

---
+++ model/Controller.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValleTemperatures.model
+{
+    class Class1
+    {
+    }
+}
