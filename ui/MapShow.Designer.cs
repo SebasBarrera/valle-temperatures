0b1dd1fad2529baf792b2da994bfcbebe85ff481
Sebastián Barrera <juan.barrera4@correo.icesi.edu.co>
2021-02-21 18:11:15 -0500

    markers logic has been implemented

--- ui/MapShow.Designer.cs
+++ ui/MapShow.Designer.cs
@@ -29,35 +29,36 @@ namespace ValleTemperatures
         /// </summary>
         private void InitializeComponent()
         {
-            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.gmap = new GMap.NET.WindowsForms.GMapControl();
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // gMapControl1
+            // gmap
             // 
-            this.gMapControl1.Bearing = 0F;
-            this.gMapControl1.CanDragMap = true;
-            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
-            this.gMapControl1.GrayScaleMode = false;
-            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
-            this.gMapControl1.LevelsKeepInMemmory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(54, 12);
-            this.gMapControl1.MarkersEnabled = true;
-            this.gMapControl1.MaxZoom = 2;
-            this.gMapControl1.MinZoom = 2;
-            this.gMapControl1.MouseWheelZoomEnabled = true;
-            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
-            this.gMapControl1.Name = "gMapControl1";
-            this.gMapControl1.NegativeMode = false;
-            this.gMapControl1.PolygonsEnabled = true;
-            this.gMapControl1.RetryLoadTile = 0;
-            this.gMapControl1.RoutesEnabled = true;
-            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
-            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
-            this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(491, 302);
-            this.gMapControl1.TabIndex = 0;
-            this.gMapControl1.Zoom = 2D;
+            this.gmap.Bearing = 0F;
+            this.gmap.CanDragMap = true;
+            this.gmap.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gmap.GrayScaleMode = false;
+            this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gmap.LevelsKeepInMemmory = 5;
+            this.gmap.Location = new System.Drawing.Point(224, 12);
+            this.gmap.MarkersEnabled = true;
+            this.gmap.MaxZoom = 20;
+            this.gmap.MinZoom = 2;
+            this.gmap.MouseWheelZoomEnabled = true;
+            this.gmap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gmap.Name = "gmap";
+            this.gmap.NegativeMode = false;
+            this.gmap.PolygonsEnabled = true;
+            this.gmap.RetryLoadTile = 0;
+            this.gmap.RoutesEnabled = true;
+            this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gmap.ShowTileGridLines = false;
+            this.gmap.Size = new System.Drawing.Size(491, 302);
+            this.gmap.TabIndex = 0;
+            this.gmap.Zoom = 6D;
+            this.gmap.Load += new System.EventHandler(this.GMap_Load);
             // 
             // button1
             // 
@@ -75,7 +76,7 @@ namespace ValleTemperatures
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(771, 554);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.gmap);
             this.Name = "MapShow";
             this.Text = "MapShow";
             this.Load += new System.EventHandler(this.Form2_Load);
@@ -85,7 +86,7 @@ namespace ValleTemperatures
 
         #endregion
 
-        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private GMap.NET.WindowsForms.GMapControl gmap;
         private System.Windows.Forms.Button button1;
     }
 }
--- ui/MapShow.cs
+++ ui/MapShow.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +15,19 @@ namespace ValleTemperatures
 {
     public partial class MapShow : Form
     {
+
+        private List<PointLatLng> Markers;
+        private GMapOverlay MarkersOverlay;
+
         public MapShow()
         {
             InitializeComponent();
+            Markers = new List<PointLatLng>();
+            MarkersOverlay = new GMapOverlay("markers");
         }
 
+        
+        
         private void Form2_Load(object sender, EventArgs e)
         {
 
@@ -25,6 +36,15 @@ namespace ValleTemperatures
         private void Cerrar(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        private void GMap_Load(object sender, EventArgs e)
+        {
+            gmap.MapProvider = GoogleMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+
+            gmap.Position = new PointLatLng(3.416667, -76.516667);
+            gmap.Overlays.Add(MarkersOverlay);
         }
     }
 }
